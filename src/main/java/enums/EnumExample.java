90e4ae6ce120b04c4c17e29f9624fd0f584c36fe
Suraj Singh <suraj.cse.ism@gmail.com>
2019-10-04 13:00:30 +0530

    enum values, retrivel and reverse-retrivel added

--- src/main/java/enums/EnumExample.java
+++ src/main/java/enums/EnumExample.java
@@ -1,8 +1,15 @@
 package enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EnumExample {
     private enum Level {
@@ -12,8 +19,7 @@ public class EnumExample {
         VERY_LOW
     }
 
-    public static void main(String []args) {
-
+    private static void doItForLevels() {
         // Print normally;
         System.out.println(Level.HIGH);
         System.out.println(Level.MEDIUM);
@@ -69,5 +75,78 @@ public class EnumExample {
         } catch (IOException e) {
             System.err.println("Input Exception.");
         }
+    }
+
+    @Data
+    private static class DrivingLicenseData {
+        private String country;
+        private String state;
+        private int cityCode;
+
+        public DrivingLicenseData(final String country, final String state, final int cityCode) {
+            this.country = country;
+            this.state = state;
+            this.cityCode = cityCode;
+        }
+    }
+
+    private enum DrivingLicensePlace {
+        KANPUR(1,"India", "UP", 72),
+        DHANBAD(2,"India", "Jharkhand", 32),
+        BANGLORE(3,"India", "Karnataka", 23),
+        LUCKNOW(4,"India", "UP", 32),
+        RANCHI(5,"India", "Bihar", 12),
+        SINGAPORE(6,"Malaysia", "Singapore", 22);
+
+        private final int Id;
+        private final String country;
+        private final String state;
+        private final int cityCode;
+
+        DrivingLicensePlace(final int Id, final String country, final String state, final int cityCode) {
+            this.Id = Id;
+            this.country = country;
+            this.state = state;
+            this.cityCode = cityCode;
+        }
+
+        private static final Map<Integer, DrivingLicensePlace> dlMap = new HashMap<>();
+        static {
+            for(DrivingLicensePlace place : DrivingLicensePlace.values()) {
+                dlMap.put(place.Id, place);
+            }
+        }
+
+        public static DrivingLicensePlace getDrivingLicensePlace(final Integer id) throws Exception {
+            if (dlMap.containsKey(id)) {
+                return dlMap.get(id);
+            } else  {
+                throw new Exception("Couldn't find what you are looking for.");
+            }
+        }
+
+        public DrivingLicenseData getDLValues() {
+            return new DrivingLicenseData(this.country, this.state, this.cityCode);
+        }
+    }
+
+    private static void doItForTypes() throws Exception {
+        DrivingLicensePlace license = DrivingLicensePlace.BANGLORE;
+        System.out.println(license + " has values => " + license.getDLValues() + " license\'s hashCode => " + license.hashCode());
+
+        license = DrivingLicensePlace.KANPUR;
+        System.out.println(license + " has values => " + license.getDLValues() + " license\'s hashCode => " + license.hashCode());
+
+        license = DrivingLicensePlace.getDrivingLicensePlace(5);
+        System.out.println(license + " has values => " + license.getDLValues() + " license\'s hashCode => " + license.hashCode());
+
+    }
+
+    public static void main(String []args) throws Exception {
+        doItForLevels();
+        for(int i=0; i<80; i++)
+            System.out.print("*");
+        System.out.print("\n");
+        doItForTypes();
     }
 }
